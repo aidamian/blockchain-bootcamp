dac634734a710f82d503b0539f4304d25e46503e
aidamian <damian@kig.ro>
2024-11-01 10:05:20 +0200

    update

--- contracts/Moneybox.sol
+++ contracts/Moneybox.sol
@@ -4,8 +4,20 @@ pragma solidity ^0.8.0;
 contract Moneybox {
 
     uint256 public ballance;
+    address private owner;
 
-    receive() external payable {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable { // 0xD197aAf1Ef2584322F191b2B72388f18718A5BC7
         ballance += msg.value;
+    }
+
+    function withdraw(uint amount, address payable destAddr) public {
+        require(ballance >= amount, "Not enough money");
+        require(msg.sender == owner, "Only owner can withdraw");
+        destAddr.transfer(amount);
+        ballance -= amount;
     }
 }
